94b629e0059bcfbcb7978a8f8aa7e5f855beb95c
GioRan <gioran0912@gmail.com>
2017-06-05 01:37:09 +0800

    Add files via upload

--- GIO/scripts/services.js
+++ GIO/scripts/services.js
@@ -2,7 +2,7 @@ window.services = {
 	//register new client
     register: function(data, success){
         $.ajax({
-            url: "http://www.hyperq.com/GIO/services/registration.php",
+            url: "./GIO/services/registration.php",
             data: data,
             type: "POST",
 			method: "POST",
@@ -17,7 +17,7 @@ window.services = {
 	//validate login client
     login: function(data, success){
         $.ajax({
-            url: "http://www.hyperq.com/GIO/services/login.php",
+            url: "./GIO/services/login.php",
             data: data,
             type: "POST",
 			method: "POST",
@@ -32,7 +32,7 @@ window.services = {
 	//get foods from db
 	getFoods: function(success){
 		$.ajax({
-            url: "http://www.hyperq.com/GIO/services/fetchItems.php",
+            url: "./GIO/services/fetchItems.php",
             type: "GET",
             success: function(data){
                 success(data);
@@ -45,7 +45,7 @@ window.services = {
 	//submit order from client
 	postOrder: function(data, success){
 		$.ajax({
-			url: "http://www.hyperq.com/GIO/services/ordering.php",
+			url: "./GIO/services/ordering.php",
 			data: data,
 			type: "POST",
 			method: "POST",
@@ -60,7 +60,7 @@ window.services = {
 	//check if user is logged in
 	checkLoginSession: function(success){
 		$.ajax({
-			url: "http://www.hyperq.com/GIO/services/checkLoginSession.php",
+			url: "./GIO/services/checkLoginSession.php",
 			type: "GET",
 			success: function(data){
 				success(data);
@@ -69,7 +69,7 @@ window.services = {
 	},
 	signOut: function(success){
 		$.ajax({
-			url: "http://www.hyperq.com/GIO/services/logout.php",
+			url: "./GIO/services/logout.php",
 			type: "GET",
 			success: function(data){
 				success(data);
