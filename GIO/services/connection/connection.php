9fe3f49d577c77476d08d4c914a55173a963301a
MaGiovylineR <palf16@gmail.com>
2017-06-10 21:04:50 +0800

    by GIO

--- GIO/services/connection/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-	mysql_select_db('db_hyperq', mysql_connect('hyperq.com', 'root','Barkero123!')) or die("db connection error");
-?>
--- GIO/services/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	if(isset($_SESSION['id'])){
-		unset($_SESSION['id']);
-		echo 1;
-	}
-?>
